be930d2f8ae0b6baa69676b4cab879b44df1d6b7
BBurakUzun <bugraburakuzun@hotmail.com>
2024-01-05 19:33:33 +0300

    Header vesaire

--- ui.py
+++ ui.py
@@ -3,22 +3,28 @@ from tkinter import *
 from tkinter import ttk, messagebox
 from tkcalendar import Calendar
 from datetime import datetime
+from ttkthemes import ThemedTk
 
 
-class Window(Tk):
+class Window(ThemedTk):
     def __init__(self):
         super().__init__()
         self.geometry("800x450")
-
+        self.set_theme_advanced("radiance", hue=2)
         # Python private verisi
         self.__goals = self.get_data_from_json()
         today = datetime.now()
 
-        self.entry = Entry(self, width=40)
-        self.entry2 = Entry(self, width=40)
-        self.add_button = Button(self, width=10, command=lambda: self.add_goal(today), text="Hedef Ekle")
-        self.delete_button = Button(self, width=10, command=self.delete_goal, text="Hedef Sil")
-        self.calendar = Calendar(self, selectmode="day", year=today.year, month=today.month, day=13, locale="tr.TR")
+        self.header = Canvas(self, background="#693177", width=800, height=50)
+        self.title_label = self.header.create_text(70, 35, text="To-Do List", font=("Times", "20", "bold"), fill="white")
+        self.entry = ttk.Entry(self, width=40)
+        self.entry2 = ttk.Entry(self, width=40)
+        self.add_label = ttk.Label(self, text="Hedeflerini Buraya Ekle !!", font=("Times", "13"))
+        self.tag_label = ttk.Label(self, text="Etiketlemek İçin Buraya !!", font=("Times", "13"))
+        self.add_button = ttk.Button(self, width=10, command=lambda: self.add_goal(today), text="Hedef Ekle")
+        self.delete_button = ttk.Button(self, width=10, command=self.delete_goal, text="Hedef Sil")
+        self.calendar = Calendar(self, selectmode="day", year=today.year, month=today.month, day=13,
+                                 locale="tr.TR", background="#824998", selectbackground="purple")
 
         self.table = ttk.Treeview(self, columns=("tarih", "hedef", "etiketler"), show="headings")
         self.table.heading("tarih", text="Tarih")
@@ -27,14 +33,18 @@ class Window(Tk):
         self.table.column("hedef", anchor="w", stretch=NO, width=133)
         self.table.heading("etiketler", text="Etiketler")
         self.table.column("etiketler", anchor="w", stretch=NO, width=133)
+
         self.table.bind("<<TreeviewSelect>>", lambda event: self.table_item_select())
         self.table_init()
 
+        self.header.place(x=0, y=0)
         self.calendar.place(x=0, y=50, width=400, height=250)
         self.table.place(x=400, y=50, width=400, height=250)
         self.add_button.place(x=400, y=330)
         self.delete_button.place(x=400, y=395)
         self.entry.place(x=75, y=335)
+        self.add_label.place(x=75, y=310)
+        self.tag_label.place(x=75, y=375)
         self.entry2.place(x=75, y=400)
 
     def add_goal(self, today: datetime):
